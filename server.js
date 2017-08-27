a4e241484aa058946129ac30056d332b9d763f19
Dhakshith <30764909+Dhakshith@users.noreply.github.com>
2017-08-27 14:18:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,24 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/article-1', function (req, res) {
+  res.send('article 1 will be served here');
+});
+app.get('/article-2', function (req, res) {
+  res.send('article 2 will be served here');
+});
+app.get('/article-3', function (req, res) {
+  res.send('article 3 will be served here');
+});
+app.get('/article-4', function (req, res) {
+  res.send('article 4 will be served here');
+});
+app.get('/article-5', function (req, res) {
+  res.send('article 5 will be served here');
+});
+app.get('/article-6', function (req, res) {
+  res.send('article 6 will be served here');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
