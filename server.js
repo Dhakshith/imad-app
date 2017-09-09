63501df1cdc41bd2d085173891e37c09d882ce5b
Dhakshith <30764909+Dhakshith@users.noreply.github.com>
2017-09-09 17:18:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var Pool = require('pg').Pool;
 var config = {
     user: 'sdhakshithraam',
     database: 'sdhakshithraam',
-    host: 'http://db.imad.hasura-app.io',
+    host: 'db.imad.hasura-app.io',
     port: '5432',
     password: process.env.DB_PASSWORD
 };
