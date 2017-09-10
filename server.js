bc145eb7e5140655699526d702de5cd5d3c3c15c
Dhakshith <30764909+Dhakshith@users.noreply.github.com>
2017-09-10 19:57:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config = {
     database: 'sdhakshithraam',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: 'db-sdhakshithraam-50031'
+    password: process.env.DB_PASSWORD
 };
 
 var app = express();
