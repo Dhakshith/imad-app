c93f9e74fdfe503f7eba6f45fb6535dce0431e13
Dhakshith <30764909+Dhakshith@users.noreply.github.com>
2017-09-11 21:52:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.get('/article-8', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-8.html'));
 });
 app.get('/article-9', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-nine.html'));
+  res.send(createTemplate(article-nine));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
