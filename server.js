0141ad2360d1e9c451632f0b45499ab9d48a6c30
Dhakshith <30764909+Dhakshith@users.noreply.github.com>
2017-09-12 17:41:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,86 +1,47 @@
-var server = require('server-js');
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
 
+var app = express();
+app.use(morgan('combined'));
 
-/*   server.use();   
-  *   
-*   here you can write the middleware code like checking whether the user has login etc.
-*   here you can use all the middlewares supported by the express webframework
-*   like for example :- 
-*      
-*     server.use(morgan());//it is a logging middleware
-*
-*   u should always write your middleware at the top request will traverse in the order
-*   you written your server so your middleware should be at the top.
-*   you can use more than one middleware like the following.
-*
-*     server.use(morgan());
-*     server.use(bodyparser());
-*     
-*      server.use(function(req,res,next)){
-*       if you are using a function as a middleware you should call next() at
-*        the end of the function otherwise the request will not reach other layers
-*        in your server.
-*      }
-*
-*    example:- 
-*              
-*               server.use(function(req,res,next){
-*                    // --middleware code--
-*                     next();               
-*               });
-*
-*    
-*    if you are handled the request in middleware based on condition like this you should not call
-*    next() after u handled the request,if you do an exception will be thrown.
-*                
-*     server.use(function(req,res,next){
-*                         
-*         if(somecondition){
-*             res.end('Error');
-*             //you should not call next() here because you already handled request
-*         }else{
-*             next(); 
-*         }                  
-*                         
-*    });
-*
-*/
-
-server.use(function(req,res,next){
-  
-  //middleware code
-  
-	next();
+app.get('/', function (req,res) {
+    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+app.get('/ui/style.css', function (req,res) {
+    res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+app.get('/article-1', function (req,res) {
+    res.sendFile(path.join(__dirname, 'ui', 'article-1.html'));
+});
+app.get('/article-2', function (req,res) {
+    res.sendFile(path.join(__dirname, 'ui', 'article-2.html'));
+});
+app.get('/article-3', function (req,res) {
+    res.sendFile(path.join(__dirname, 'ui', 'article-3.html'));
+});
+app.get('/article-4', function (req,res) {
+    res.sendFile(path.join(__dirname, 'ui', 'article-4.html'));
+});
+app.get('/article-5', function (req,res) {
+    res.sendFile(path.join(__dirname, 'ui', 'article-5.html'));
+});
+app.get('/article-6', function (req,res) {
+    res.sendFile(path.join(__dirname, 'ui', 'article-6.html'));
+});
+app.get('/article-7', function (req,res) {
+    res.sendFile(path.join(__dirname, 'ui', 'article-7.html'));
+});
+app.get('/article-8', function (req,res) {
+    res.sendFile(path.join(__dirname, 'ui', 'article-8.html'));
+});
+var counter = 0;
+app.get('/counter', function (req,res) {
+    counter = counter + 1;
+    res.send(counter.toString())
 });
 
-server.static(__dirname);
-
-server.static(__dirname+'/test');
-
-
-server.get('/path', function(req, res) {
-	res.end('path get');
-})
-
-server.get('/path/:id', function(req, res) {
-	res.end('path get with param' + req.params.id);
+var port = 8080;
+app.listen(8080, function () {
+    console.log('IMAD course app running on port 8080!');
 });
-
-server.route('/route').get(function(req, res) {
-	res.end('received');
-}).put(function(req, res) {
-	res.end('received put');
-}).delete(function(req, res) {
-	res.end('received delete');
-});
-
-server.route('/route2').get(function(req, res) {
-	res.end('received route2 get');
-}).put(function(req, res) {
-	res.end('route put');
-});
-
-server.start().listen(3000, function() {
-	console.log('server started');
-});
-
