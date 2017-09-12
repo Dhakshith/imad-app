0dfd0065ce9dda86e987506f696fca8d469187f5
Dhakshith <30764909+Dhakshith@users.noreply.github.com>
2017-09-12 14:27:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ var img = document.getElementById('pokemon');
 img.onclick = function () {
     img.style.marginLeft = '500px';
 };
+
+
+var img = document.getElementById('pokeball');
+img.onclick = function () {
+    img.style.marginLeft = '500px';
+};
