e54701eb6fd7c7474c62531721e144b212713c31
Dhakshith <30764909+Dhakshith@users.noreply.github.com>
2017-09-12 20:00:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.get('/submit-name/:name', function (req,res) {
 var counter = 0;
 app.get('/counter', function (req,res) {
     counter = counter + 1;
-    res.send(counter.toString())
+    res.send(counter.toString());
 });
 
 var port = 8080;
