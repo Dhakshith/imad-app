889cd6e547d4ef0dd4519aaceead9d949ab05579
Dhakshith <30764909+Dhakshith@users.noreply.github.com>
2017-09-13 08:16:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -46,9 +46,6 @@ app.get('/article-7', function (req,res) {
 app.get('/article-8', function (req,res) {
     res.sendFile(path.join(__dirname, 'ui', 'article-8.html'));
 });
-app.get('/article-9', function (req,res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-9.html'));
-});
 
 var port = 8080;
 app.listen(8080, function () {
