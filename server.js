8a793e0e6d59a4461786467364ac2c117a50314d
Dhakshith <30764909+Dhakshith@users.noreply.github.com>
2017-09-15 19:03:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req,res) {
-    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+    res.sendFile(path.join(dirname, 'ui', 'index.html'));
 });
 var counter = 0;
 app.get('/counter', function (req,res) {
@@ -17,31 +17,31 @@ app.get('/article-1', function (req,res) {
     res.sendFile(path.join(dirname, 'ui', 'article-1.html'));
 });
 app.get('/article-2', function (req,res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-2.html'));
+    res.sendFile(path.join(dirname, 'ui', 'article-2.html'));
 });
 app.get('/article-3', function (req,res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-3.html'));
+    res.sendFile(path.join(dirname, 'ui', 'article-3.html'));
 });
 app.get('/article-4', function (req,res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-4.html'));
+    res.sendFile(path.join(dirname, 'ui', 'article-4.html'));
 });
 app.get('/article-5', function (req,res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-5.html'));
+    res.sendFile(path.join(dirname, 'ui', 'article-5.html'));
 });
 app.get('/article-6', function (req,res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-6.html'));
+    res.sendFile(path.join(dirname, 'ui', 'article-6.html'));
 });
 app.get('/article-7', function (req,res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-7.html'));
+    res.sendFile(path.join(dirname, 'ui', 'article-7.html'));
 });
 app.get('/article-8', function (req,res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-8.html'));
+    res.sendFile(path.join(dirname, 'ui', 'article-8.html'));
 });
 app.get('/MyProfile', function (req,res) {
-    res.sendFile(path.join(__dirname, 'ui', 'Profile.html'));
+    res.sendFile(path.join(dirname, 'ui', 'Profile.html'));
 });
 
 var port = 8080;
 app.listen(8080, function () {
-    
+    "IMAD course app listening on port 8080";
 });
