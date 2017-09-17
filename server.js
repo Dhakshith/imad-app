d920aeffa2c06ccdb95ff0d217624fb902292bfa
Dhakshith <30764909+Dhakshith@users.noreply.github.com>
2017-09-17 20:40:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,40 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+var app = express();
+app.use(morgan('combined'));
+
+var articleTwo = {
+    content: `<!DOCTYPE html>
+<html>
+    <link href="/ui/style.css" rel= "stylesheet" />
+    <body>
+        <p style="font-size:75px;"><strong>Article-1 | Dhakshith</strong>
+        <meta name="viewport" content="width=device-width, initial+scale=1" />
+        <div>
+            <p style="font-size:37px;"><a href="/">Home</a>
+        </div>
+        <hr/>
+        <button type="button" onclick="document.getElementById('demo').innerHTML = Date()">Click me to display Date and Time.</button>
+        <p id="demo"></p>
+        <div>
+            <p style="font-size:50px;">
+                <strong><b>Hi!This is my 1st article</b></strong>
+        </div>
+        <img id = 'pokemon' src="https://res.cloudinary.com/lmn/image/upload/fl_lossy,q_80/f_auto/v1/gameskinny/d063d2b775dee12a8cc73f05d7b9b589.png" class="img-medium"/>
+    </body>
+</html>`
+};
+
+function addTemplate (data) {
+    var content = data.content;
+    
+    var XmlTemplate = `
+        ${content}
+    `;
+    return XmlTemplate;
+}
+
 var articleOne = {
     content: `<!DOCTYPE html>
               <html>
@@ -39,14 +73,11 @@ var articleOne = {
 function createTemplate (data) {
     var content = data.content;
     
-    var htmlTemplate = `
+    var hmlTemplate = `
         ${content}
     `;
     return htmlTemplate;
 }
-
-var app = express();
-app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -56,6 +87,9 @@ app.get('/style.css', function (req, res) {
 });
 app.get('/article-1', function (req, res) {
   res.send(createTemplate(articleOne));
+});
+app.get('/article-2', function (req, res) {
+  res.send(addTemplate(articleTwo));
 });
   
 // Do not change port, otherwise your app won't run on IMAD servers
