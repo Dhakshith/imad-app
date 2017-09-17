07dff34b8e01502b6cd61c0adf1fbe5b496b4373
Dhakshith <30764909+Dhakshith@users.noreply.github.com>
2017-09-17 21:17:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,131 +5,111 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleThree = {
-    content: `<!DOCTYPE html>
-<html>
-    <head>
-        <title>
-            Article-3 | Dhakshith
-        </title>
-        <link href="/style.css" rel= "stylesheet" />
-    </head>
-    <body>
-        <div class="container">
-        <p style="font-size:75px;"><strong>Article-3 | Dhakshith</strong>
-        <meta name="viewport" content="width=device-width, initial+scale=1" />
-        <div>
-            <p style="font-size:37px;"><a href="/">Home</a>
-        </div>
-        <hr/>
-        <p style="font-size:30px;color:red;font-family:arial"><span id="datetime"></span></p>
-                            <script>
-                                var dt = new Date();
-                                document.getElementById("datetime").innerHTML = "September 17 2017";
-                            </script>
-        <p id="demo"></p>
-        <div>
-            <p style="font-size:50px;">
-                <strong><b>Hi!This is my 1st article</b></strong>
-        </div>
-        <img id = 'pokemon' src="https://s3-ap-southeast-2.amazonaws.com/images.assignment1.fit3140/anime/charizard/tumblr_mt8kfgi0TF1s29bhho1_500.gif" class="img-medium"/><br><br>
-    </body>
-</html>`
-};
-
-function delTemplate (data) {
-    var content = data.content;
-    
-    var aTemplate = `
-        ${content}
-    `;
-    return aTemplate;
-}
-
-
-var articleTwo = {
-    content: `<!DOCTYPE html>
-<html>
-    <head>
-        <title>
-            Article-2 | Dhakshith
-        </title>
-        <link href="/style.css" rel= "stylesheet" />
-    </head>
-    <body>
-        <div class="container">
-        <p style="font-size:75px;"><strong>Article-2 | Dhakshith</strong>
-        <meta name="viewport" content="width=device-width, initial+scale=1" />
-        <div>
-            <p style="font-size:37px;"><a href="/">Home</a>
-        </div>
-        <hr/>
-        <p style="font-size:30px;color:red;font-family:arial"><span id="datetime"></span></p>
-                            <script>
-                                var dt = new Date();
-                                document.getElementById("datetime").innerHTML = "September 17 2017";
-                            </script>
-        <p id="demo"></p>
-        <div>
-            <p style="font-size:50px;">
-                <strong><b>Hi!This is my 1st article</b></strong>
-        </div>
-        <img id = 'pokemon' src="http://25.media.tumblr.com/tumblr_maxdexHDGJ1qcm0wfo1_500.gif" class="img-medium"/><br><br>
-    </body>
-</html>`
-};
-
-function addTemplate (data) {
-    var content = data.content;
-    
-    var XmlTemplate = `
-        ${content}
-    `;
-    return XmlTemplate;
-}
-
-var articleOne = {
-    content: `<!DOCTYPE html>
-              <html>
-                    <head>    
-                        <link href="/ui/style.css" rel= "stylesheet" />
-                        <title>
-                            Article-1 | Dhakshith
-                        </title>
-                        <link href="style.css" rel="stylesheet" />
-                    </head>
-                    <body>
-                        <div class="container">                    
-                            <p style="font-size:75px;"><strong>Article-1 | Dhakshith</strong>
-                            <meta name="viewport" content="width=device-width, initial-scale=1" />
+var articles = {
+    articleThree : {
+        content: `
+                    <!DOCTYPE html>
+                    <html>
+                        <head>
+                            <title>
+                                Article-3 | Dhakshith
+                            </title>
+                            <link href="/style.css" rel= "stylesheet" />
+                        </head>
+                        <body>
+                            <div class="container">
+                            <p style="font-size:75px;"><strong>Article-3 | Dhakshith</strong>
+                            <meta name="viewport" content="width=device-width, initial+scale=1" />
                             <div>
                                 <p style="font-size:37px;"><a href="/">Home</a>
                             </div>
-                            <p style="font-size:30px;color:red;font-family:arial"><span id="datetime"></span></p>
-                            <script>
-                                var dt = new Date();
-                                document.getElementById("datetime").innerHTML = "September 17 2017";
-                            </script>
                             <hr/>
-                            </p>
+                            <p style="font-size:30px;color:red;font-family:arial"><span id="datetime"></span></p>
+                                                <script>
+                                                    var dt = new Date();
+                                                    document.getElementById("datetime").innerHTML = "September 17 2017";
+                                                </script>
+                            <p id="demo"></p>
                             <div>
                                 <p style="font-size:50px;">
-                                    <strong><b>Hi !!! This is my 1st article</b></strong>
+                                    <strong><b>Hi!This is my 1st article</b></strong>
                             </div>
-                            <img id = 'pokemon' src="https://res.cloudinary.com/lmn/image/upload/fl_lossy,q_80/f_auto/v1/gameskinny/d063d2b775dee12a8cc73f05d7b9b589.png" class="img-medium"/><br><br>
-                        </div>
-                    </body>
-              </html>`
+                            <img id = 'pokemon' src="https://s3-ap-southeast-2.amazonaws.com/images.assignment1.fit3140/anime/charizard/tumblr_mt8kfgi0TF1s29bhho1_500.gif" class="img-medium"/><br><br>
+                        </body>
+                    </html>`
+    },
+    articleTwo : {
+        content: `<!DOCTYPE html>
+    <html>
+        <head>
+            <title>
+                Article-2 | Dhakshith
+            </title>
+            <link href="/style.css" rel= "stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+            <p style="font-size:75px;"><strong>Article-2 | Dhakshith</strong>
+            <meta name="viewport" content="width=device-width, initial+scale=1" />
+            <div>
+                <p style="font-size:37px;"><a href="/">Home</a>
+            </div>
+            <hr/>
+            <p style="font-size:30px;color:red;font-family:arial"><span id="datetime"></span></p>
+                                <script>
+                                    var dt = new Date();
+                                    document.getElementById("datetime").innerHTML = "September 17 2017";
+                                </script>
+            <p id="demo"></p>
+            <div>
+                <p style="font-size:50px;">
+                    <strong><b>Hi!This is my 1st article</b></strong>
+            </div>
+            <img id = 'pokemon' src="http://25.media.tumblr.com/tumblr_maxdexHDGJ1qcm0wfo1_500.gif" class="img-medium"/><br><br>
+        </body>
+    </html>`
+    },
+    articleOne : {
+        content: `<!DOCTYPE html>
+                  <html>
+                        <head>    
+                            <link href="/ui/style.css" rel= "stylesheet" />
+                            <title>
+                                Article-1 | Dhakshith
+                            </title>
+                            <link href="style.css" rel="stylesheet" />
+                        </head>
+                        <body>
+                            <div class="container">                    
+                                <p style="font-size:75px;"><strong>Article-1 | Dhakshith</strong>
+                                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                                <div>
+                                    <p style="font-size:37px;"><a href="/">Home</a>
+                                </div>
+                                <p style="font-size:30px;color:red;font-family:arial"><span id="datetime"></span></p>
+                                <script>
+                                    var dt = new Date();
+                                    document.getElementById("datetime").innerHTML = "September 17 2017";
+                                </script>
+                                <hr/>
+                                </p>
+                                <div>
+                                    <p style="font-size:50px;">
+                                        <strong><b>Hi !!! This is my 1st article</b></strong>
+                                </div>
+                                <img id = 'pokemon' src="https://res.cloudinary.com/lmn/image/upload/fl_lossy,q_80/f_auto/v1/gameskinny/d063d2b775dee12a8cc73f05d7b9b589.png" class="img-medium"/><br><br>
+                            </div>
+                        </body>
+                  </html>`
+    };
 };
-
 function createTemplate (data) {
     var content = data.content;
-    
     var htmlTemplate = `
-        ${content}
-    `;
-    return htmlTemplate;
-}
+            ${content}
+        `;
+        return htmlTemplate;
+    }
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -141,10 +121,10 @@ app.get('/article-1', function (req, res) {
   res.send(createTemplate(articleOne));
 });
 app.get('/article-2', function (req, res) {
-  res.send(addTemplate(articleTwo));
+  res.send(createTemplate(articleTwo));
 });
 app.get('/article-3', function (req, res) {
-  res.send(delTemplate(articleThree));
+  res.send(createTemplate(articleThree));
 });
   
 // Do not change port, otherwise your app won't run on IMAD servers
