c2788d07ca828be4a56efce3929009a75df54e6e
Dhakshith <30764909+Dhakshith@users.noreply.github.com>
2017-09-18 16:09:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,8 +3,8 @@ var img = document.getElementById('pokemon');
 var marginLeft = 0;
 function moveRight () {
     marginLeft = marginLeft + 0.5;
-    marginLeft = 450;
     img.style.marginLeft = marginLeft + 'px';
+    marginLeft = 450;
 }
 img.onclick = function () {
     var interval = setInterval(moveRight, 9);
