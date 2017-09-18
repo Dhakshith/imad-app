768af7870b0a149ca91a006bfdae66dbd53de27b
Dhakshith <30764909+Dhakshith@users.noreply.github.com>
2017-09-18 22:35:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -49,6 +49,23 @@ profile = {
             <input type="submit" id="submit_btn" value="Submit"></input>
             <script type="text/javascript" src="/main.js">
             </script>
+            <script>
+                var button = document.getElementById('counter');
+button.onclick = function () {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML= counter.toString();
+            }
+        }
+    };
+    request.open('GET', 'http://sdhakshithraam.imad.hasura-app.io/counter', true);
+    request.send(null);
+};
+            </script>
             <div style="color: #FF0A0E;font-size: 30px">
                 <ul id="namelist">
                 </ul>
