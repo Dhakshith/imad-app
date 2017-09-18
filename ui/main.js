b504c598db84a6798052743fae939b04fce53000
Dhakshith <30764909+Dhakshith@users.noreply.github.com>
2017-09-18 13:52:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
-$("img").click(function(){
+$("pokemon").click(function(){
     var div = $("img");
     div.animate({height: '300px', opacity: '0.4'}, "slow");
     div.animate({width: '300px', opacity: '0.8'}, "slow");
