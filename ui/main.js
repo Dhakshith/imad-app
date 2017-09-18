064bd60c1f09fa7652d3a1a7dba8862ecd1b0694
Dhakshith <30764909+Dhakshith@users.noreply.github.com>
2017-09-18 16:32:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,20 @@ function moveRight () {
     marginLeft = marginLeft + 0.5;
     img.style.marginLeft = marginLeft + 'px';
 }
+img.onclick = function () {
+    var interval = setInterval(moveRight, 9);
+};
+$(document).ready(function(){
+    $("#btn1").click(function(){
+        $("#box").animate({height: "+300px"});
+    });
+    $("#btn2").click(function(){
+        $("#box").animate({height: "100px"});
+    });
+    $("#btn3").click(function(){
+        $("#box").animate({width: "+300"});
+    });
+    $("#btn4").click(function(){
+        $("#box").animate({width: "100px"});
+    });
+});
