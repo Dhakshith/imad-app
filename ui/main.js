42ad02a197d68e95e2597fe6beb4b01c54df1c3e
Dhakshith <30764909+Dhakshith@users.noreply.github.com>
2017-09-18 19:43:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,7 @@ img.onclick = function () {
 };
 
 var button = document.getElementById('counter');
+var counter = 0;
 button.onclick = function () {
     counter = counter + 1;
     var span = document.getElementById('count');
