8882c031ff9177020fd7ceee1a072e93e0421fd3
Dhakshith <30764909+Dhakshith@users.noreply.github.com>
2017-09-18 18:24:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ profile = {
     <!DOCTYPE html>
     <html>
     <head>
-    <link href="/style.css" rel="stylesheet"><title>My Profile</title><link rel="icon" type="image/png" href="https://vignette2.wikia.nocookie.net/leonhartimvu/images/d/d8/145_Zapdos_DW.png/revision/latest?cb=20131218041858" />
+    <link href="/style.css" rel="stylesheet"><title>My Profile</title><link rel="icon" type="image/gif" href="https://vignette2.wikia.nocookie.net/leonhartimvu/images/d/d8/145_Zapdos_DW.png/revision/latest?cb=20131218041858" />
     </head>
     <body><div class="container">
             <meta name="viewport" content="width=device-width, initial-scale=1" />
