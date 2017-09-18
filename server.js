0bfbbfbf686b62059b5524d2259fd1b29bf26ff3
Dhakshith <30764909+Dhakshith@users.noreply.github.com>
2017-09-18 15:10:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -145,7 +145,7 @@ function createTemplate (data) {
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/main', function (req, res) {
+app.get('/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 app.get('/style.css', function (req, res) {
