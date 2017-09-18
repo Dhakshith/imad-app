9ac2d63c5efb3cb0f6a16b80508904ba2db06e7c
Dhakshith <30764909+Dhakshith@users.noreply.github.com>
2017-09-18 22:16:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,7 @@
-var button = document.getElementById('counter');
-button.onclick = function () {
-    
+var nameInput = document.getElementById('name');
+                var name = nameInput.value;
+                var submit = document.getElementById('submit_btn');
+                submit.onclick = function () {
     
     var request = new XMLHttpRequest();
     
@@ -9,15 +10,19 @@ button.onclick = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
             
             if (request.status === 200) {
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for (i=0; i< names.length; i++) {
+                    list += '<li>' + names[i] + '</li>';
+                    }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
             }
         }
-        
     };
     
     
-    request.open('GET', 'http://sdhakshithraam.imad.hasura-app.io/counter', true);
+    request.open('GET', 'http://sdhakshithraam.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
 };
