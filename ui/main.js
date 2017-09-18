bb86b30906cefffb653dc280ac6dea70bdd86a1b
Dhakshith <30764909+Dhakshith@users.noreply.github.com>
2017-09-18 15:31:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,6 @@
 console.log('Loaded!');
 var img = document.getElementById('pokemon');
 img.onclick = function () {
-        img.animate({height: '300px', opacity: '0.4'}, "slow");
-        img.animate({width: '300px', opacity: '0.8'}, "slow");
-        img.animate({height: '100px', opacity: '0.4'}, "slow");
-        img.animate({width: '100px', opacity: '0.8'}, "slow");
+        img.style.marginLeft = '100px';
+        img.style.marginRight = '100px';
 };
