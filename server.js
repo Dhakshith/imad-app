4d9ce38f86aa2e7c638250ce4d87df9fb49ea2b2
Dhakshith <30764909+Dhakshith@users.noreply.github.com>
2017-09-18 22:17:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -53,20 +53,6 @@ profile = {
                 <ul id="namelist">
                 </ul>
             </div>
-            <script>
-                var nameInput = document.getElementById('name');
-                var name = nameInput.value;
-                var submit = document.getElementById('submit_btn');
-                submit.onclick = function () {
-                    var names = ['name1', 'name2', 'name3', 'name4', 'name5'];
-                    var list = '';
-                    for (i=0; i< names.length; i++) {
-                        list += '<li>' + names[i] + '</li>';
-                    }
-                    var ul = document.getElementById('namelist');
-                    ul.innerHTML = list;
-                };
-            </script>
         </div>
     </body>
 </html>
