133aa0a06dc18505cb1385426f66ae3404c33a32
Dhakshith <30764909+Dhakshith@users.noreply.github.com>
2017-09-19 20:51:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ button.onclick = function () {
 };
             </script>
             <div style="color: #FF0A0E;font-size: 30px">
-                <ul style="list-style-image: url('sqpurple.gif');" id="namelist">
+                <ul class="a" id="namelist">
                 </ul>
             </div>
         </div>
