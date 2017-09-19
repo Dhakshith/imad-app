35b823a3a80e1c32af76d608c29ea66e0d49d6e8
Dhakshith <30764909+Dhakshith@users.noreply.github.com>
2017-09-19 19:38:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -418,6 +418,20 @@ app.get('/article-7', function (req, res) {
 app.get('/article-8', function (req, res) {
   res.send(createTemplate(articleEight));
 });
+app.get('/article-9', function (req, res) {
+  Pool.query("select * from articles where id = '1'", function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          if (result.rows.length === 0) {
+              res.status(404).send('Sorry! Article Not Found!');
+          } else {
+              var articlesData = result.rows[0];
+              res.send(createTemplate(articlesData));
+          }
+      }
+  });
+});
 app.get('/MyProfile', function (req, res) {
     res.send(createTemplate(profile));
 });
