bcdf64efbe868267b343b879afb90259f8f79569
Dhakshith <30764909+Dhakshith@users.noreply.github.com>
2017-09-19 22:08:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -378,7 +378,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 function hash(input, salt) {
-    var hashed = crypto.pbkdf2Sync(input, salt, 0, 512, 'sha512');
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
     return hashed.toString('hex');
     
 }
