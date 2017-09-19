6ddf76dba389e1b33409ceb658767b97a163e1c4
Dhakshith <30764909+Dhakshith@users.noreply.github.com>
2017-09-19 19:15:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+var config = {
+    user: 'sdhakshithraam',
+    database: 'sdhakshithraam',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
 profile = {
@@ -373,6 +381,18 @@ app.get('/main.js', function (req, res) {
 });
 app.get('/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+var Pool = new Pool(config);
+app.get('/Database', function (req, res) {
+  //make a select request
+  //return a response with the results
+  pool.query('SELECT * FROM TestApp', function (err, result){
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send(JSON.stringify(result));
+      }
+  });
 });
 app.get('/article-1', function (req, res) {
   res.send(createTemplate(articleOne));
