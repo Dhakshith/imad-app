2aace4347da4ce04bd2dee09182588a4e954fb0b
Dhakshith <30764909+Dhakshith@users.noreply.github.com>
2017-09-20 12:49:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ button.onclick = function () {
             </h6>
             <form method="post" action="index.html">
                 <input style="color:#80334C;font-size:30px" type="text" id="username" placeholder="Username"></input><br><br><br>
-                <input style="color:#8CFFAB;font-size:30px" type="password" id="password" placeholder="Password"></input>
+                <input style="color:#8CFFAB;font-size:30px" type="password" id="password" placeholder="Password"></input><br><br><br>
                 <input style="color:#FFB842;font-size:30px" type="submit" id="submit_btn" Value="Submit"></input>
                 <br><br><br>
             </form>
