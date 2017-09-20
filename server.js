6899f8aa1aae4d85d5e46eacce6a529b9ac87912
Dhakshith <30764909+Dhakshith@users.noreply.github.com>
2017-09-20 12:08:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var config = {
     password: process.env.DB_PASSWORD
 };
 var app = express();
-app.use(bodyParser.json)(morgan('combined'));
+app.use.morgan('combined');
 profile = {
     content: `
     <!DOCTYPE html>
