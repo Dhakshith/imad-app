fab3a11c14cabe133c3aa5c5d8be81d16a49c35c
Dhakshith <30764909+Dhakshith@users.noreply.github.com>
2017-09-28 21:29:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -37,8 +37,8 @@ progress = {
     		<br><br><hr>
     	    <script type="text/javascript" src="/main.js"></script>
     	    <h1 style="font-size:50px;color:#FFA300">Circular Progress Bar</h1>
-    	    <script src="http://code.jquery.com/jquery-3.2.1.js"></script>
-    	    <script src="plugin.js"></script>
+    	    <script src="//code.jquery.com/jquery.min.js"></script>
+            <script src="scripts/plugin.js"></script>
     	    <div class="my-progress-bar"></div>
             <script type="text/javascript">
                 var progress_circle = $(".my-progress-bar").gmpc({
