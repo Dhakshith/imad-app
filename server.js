4b04c143dac1dc47dc4d72addef810d4dece5949
Dhakshith <30764909+Dhakshith@users.noreply.github.com>
2017-09-29 11:40:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ loginoutcom = {
                 <label>Your Comment: <br>
                 <textarea rows="5" cols="45" placeholder="Your Comment..."></textarea>
                 </label><br>
-                <button class="button" type="submit" name="Submit">
+                <button class="button" type="submit" value="Submit">
             </form>
             <br><hr>
         </div>
