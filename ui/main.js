d9d974a9d0f96a79c19c6cd0be8ebde69097ffa5
Dhakshith <30764909+Dhakshith@users.noreply.github.com>
2017-10-04 19:42:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -41,8 +41,3 @@ function move() {
     }
   }
 }
-$('.second.circle').circleProgress({
-    value: 0.6
-  }).on('circle-animation-progress', function(event, progress) {
-    $(this).find('strong').html(Math.round(100 * progress) + '<i>%</i>');
-  });
