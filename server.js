a0cd15cc1db4d2ac0ad06defe2c9af64e40dfe2d
Dhakshith <30764909+Dhakshith@users.noreply.github.com>
2017-11-30 08:26:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ progress = {
     	<head>	
     		<meta charset="UTF-8">
     		<title>ProgressBar+Spinners</title>
-    		<link href="/style.css" rel="stylesheet"><title>My Profile</title><link rel="icon" type="image/gif" href="https://i.pinimg.com/736x/bf/91/8f/bf918fed99e248e964fa1a2bdc2d8e05--logo-branding-branding-design.jpg" />
+    		<title>My Profile</title>
+    		<link rel="icon" type="image/gif" href="https://i.pinimg.com/736x/bf/91/8f/bf918fed99e248e964fa1a2bdc2d8e05--logo-branding-branding-design.jpg" />
     		<link href="/style.css" rel="stylesheet" />
     	</head>
     	<body>
@@ -51,6 +52,7 @@ Clock = {
     	<title>Clock</title>
     	<meta http-equiv="X-UA-Compatible" content="IE=edge">
     	<meta name="viewport" content="width=device-width, initial-scale=1">
+    	<link href="/style.css" rel="stylesheet" />
     </head>
     <body>
     	<style type="text/css">
